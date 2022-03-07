2a5f85e571b7777d8f48d1e94bcb823ee7ae2b32
hposedev <hposedev@gmail.com>
2022-03-07 15:48:20 +0100

    added intent

--- app/src/main/java/com/example/wordsapp/LetterAdapter.kt
+++ app/src/main/java/com/example/wordsapp/LetterAdapter.kt
@@ -66,7 +66,8 @@ class LetterAdapter :
         holder.button.setOnClickListener {
             val context = holder.view.context
             val intent = Intent(context, DetailActivity::class.java)
-            intent.putExtra("letter", holder.button.toString())
+            intent.putExtra("letter", holder.button.text.toString())
+            context.startActivity(intent)
         }
     }
 
