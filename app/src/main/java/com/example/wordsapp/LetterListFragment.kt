391fa26fe9a0f4c54b98026dd298f849318a30ec
hposedev <hposedev@gmail.com>
2022-03-09 19:36:03 +0100

    added fragments

---
+++ app/src/main/java/com/example/wordsapp/LetterListFragment.kt
@@ -0,0 +1,13 @@
+package com.example.wordsapp
+
+import androidx.fragment.app.Fragment
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [LetterListFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class LetterListFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+
+}
---
+++ app/src/main/java/com/example/wordsapp/WordListFragment.kt
@@ -0,0 +1,16 @@
+package com.example.wordsapp
+
+import androidx.fragment.app.Fragment
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [WordListFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class WordListFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+
+}
